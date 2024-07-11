4d7d090b9767cd0cb648b27058310a5a7cdd8acd
Yash Goyal <yashgoyal4321@gmail.com>
2024-07-11 16:30:13 +0530

    mobile nav fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 
 //components
-import Logo from './Logo';
+
 import ThemeToggler from './ThemeToggler';
 import Nav from './Nav';
 import MobileNav from './MobileNav';
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -2,7 +2,7 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { AlignJustify } from "lucide-react";
 
 import Nav from "./Nav";
-import Logo from "./Logo";
+
 import Socials from "./Socials";
 
 const MobileNav = () => {
@@ -14,7 +14,7 @@ const MobileNav = () => {
          <SheetContent>
             <div className="flex flex-col items-center justify-between h-full py-8">
                <div className="flex flex-col items-center gap-y-32">
-                  <Logo />
+                  
                   <Nav
                      containerStyles="flex flex-col items-center gap-y-6"
                      linkStyles="text-2xl"
