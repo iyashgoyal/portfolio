43145f7ca80389b845b6023cbe6b82d137316e35
Yash Goyal <yashgoyal4321@gmail.com>
2025-07-20 18:48:11 +0530

    alignment fix

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,17 +4,17 @@ import { MailIcon, HomeIcon, PhoneCall } from "lucide-react";
 const Contact = () => {
   return (
     <section>
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex flex-col items-center">
         {/* texto e ilustracion */}
-        <div className="grid lg:grid-cols-2 pt-12 lg:h-[480px] mb-6 lg:mb-24">
+        <div className="grid lg:grid-cols-2 pt-12 lg:h-[480px] mb-6 lg:mb-24 w-full">
           {/* texto */}
-          <div className="flex flex-col justify-center pl-[50px] lg:pl-[150px]">
+          <div className="flex flex-col justify-center items-center lg:items-start px-4 lg:pl-[150px]">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary"></span>
               Hello Developer 👋
             </div>
-            <h1 className="h1 max-w-md mb-8">Let's Work Together</h1>
-            <p className="subtitle max-w-[400px]">
+            <h1 className="h1 max-w-md mb-8 text-center lg:text-left">Let's Work Together</h1>
+            <p className="subtitle max-w-[400px] text-center lg:text-left">
               Drop your details for a quick connect
             </p>
           </div>
@@ -22,9 +22,9 @@ const Contact = () => {
           <div className="hidden lg:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
         </div>
         {/* info texto y formulario */}
-        <div className="grid lg:grid-cols-2 mb-24 lg:mb-32 items-start">
+        <div className="grid lg:grid-cols-2 mb-24 lg:mb-32 items-start w-full">
           {/* info texto */}
-          <div className="flex flex-col gap-y-4 lg:gap-y-14 mb-12 lg:mb-24 text-base lg:text-lg pl-[50px] lg:pl-[150px]">
+          <div className="flex flex-col gap-y-4 lg:gap-y-14 mb-12 lg:mb-24 text-base lg:text-lg px-4 lg:pl-[150px]">
             {/* mail */}
             <div className="flex items-center gap-x-8">
               <MailIcon size={18} className="text-primary" />
